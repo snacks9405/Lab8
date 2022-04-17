6fdf58b9242303e38f99970551f08590ac215da9
snacks9405 <93486382+snacks9405@users.noreply.github.com>
2022-04-16 20:35:19 -0500

    Final

--- src/HashMapPlayground.java
+++ src/HashMapPlayground.java
@@ -2,40 +2,56 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class HashMapPlayground {
-    double lambda = 0.10;
-    Random rand = new Random(42);
+    double lambda = 0.10; //beginning lambda value
+    Random rand = new Random(42); //seeded rng
     
-
+    /**
+     * drives mechanism to create and print experimental data to the terminal
+     * 
+     * @param N size of table to test
+     */
     public void printExperimentalResultsTable(int N) {
-        ArrayList<ArrayList<Double>> dataList = new ArrayList<>();
+        ArrayList<ArrayList<Double>> dataList = new ArrayList<>(); //stores data to print here
         for (double i = lambda; i <= 0.9; i += .1) {
-            dataList.add(dataList.size(), generateData(i, N));
+            dataList.add(dataList.size(), generateData(i, N)); //passes lambda and table size to generateData(), stores in array
         }
-        printToTerminal(dataList);
-    }
+        printToTerminal(dataList); //sends complete data to printToTerminal() for final printing step
+    }//printExperimentalResultsTable method
 
-    public void printToTerminal(ArrayList<ArrayList<Double>> values) {
+    /**
+     * accepts an array of data and prints to terminal with appropriate format
+     * each ArrayList within ArrayList contains two values, first: lambda value, second: average miss cost
+     * @param data data to print
+     */
+    public void printToTerminal(ArrayList<ArrayList<Double>> data) {
         System.out.println(" L        Empirical   Theoretical");
         System.out.println("            ASMC         ASMC");
-        for (ArrayList<Double> lineData : values) {
-            double theoreticalValue = 0.5 * (1.0 + (1.0 / Math.pow((1-lineData.get(0)), 2)));
+        for (ArrayList<Double> lineData : data) {
+            double theoreticalValue = 0.5 * (1.0 + (1.0 / Math.pow((1-lineData.get(0)), 2))); //takes current lambda and calculates theoretical
             System.out.printf("%-10.1f %6.3f %11.3f\n", lineData.get(0), lineData.get(1), theoreticalValue);
         }
-    }
+    }//printToTerminal method
 
+    /**
+     * generates empirical average miss cost on given lambda and table size by averagine
+     * 1000 runs
+     * 
+     * @param lambda current lambda value
+     * @param N tablesize
+     * @return ArrayList with current lambda and average miss cost
+     */
     public ArrayList<Double> generateData(double lambda, int N) {
         ArrayList<Double> lineData = new ArrayList<>();
         double averageCostToSearch = 0;
         for (int i = 0; i <= 1000; i++) {
             TitanProbeHashMap<Integer, Integer> mapToTest = new TitanProbeHashMap<>(N);
-            for (int j = 0; j < lambda * N; j++) {
+            for (int j = 0; j < lambda * N; j++) { //fills hashmap with lambda*N values
                 mapToTest.put(rand.nextInt(), j);
             }
-            averageCostToSearch += mapToTest.empiricalAverageSearchMissCost();
+            averageCostToSearch += mapToTest.empiricalAverageSearchMissCost(); //combines average cost 1000x
         }
-        lineData.add(0, lambda);
-        lineData.add(1, averageCostToSearch/1000);
+        lineData.add(0, lambda); //stores lambda in first position
+        lineData.add(1, averageCostToSearch/1000); //stores average miss cost in second position
         return lineData;
-    }
-
-}
+    }//generateData method
+}//HashMapPlayground class
--- src/TitanProbeHashMap.java
+++ src/TitanProbeHashMap.java
@@ -1,51 +1,68 @@
 public class TitanProbeHashMap<K, V> extends LinearProbingHashTable<K, V> {
 
+    /**
+     * constructor to include capacity specification
+     * 
+     * @param capacity
+     */
     public TitanProbeHashMap(int capacity) {
-        tableSize = capacity;
-        size = 0;
-        keys = (K[]) new Object[tableSize];
-        values = (V[]) new Object[tableSize];
-    }
+        super(capacity);
+    }//TitanProbeHashMap constructor
 
+    /**
+     * returns a string representing the hashmap with empty cells : E and 
+     * occupied cells : O
+     */
     public String toString() {
         StringBuilder sBuilder = new StringBuilder();
-
         for (int i = 0; i < tableSize; i++) {
             if (i % 79 == 0 && i != 0) {
-                sBuilder.append("    " + i + "     \n");
+                sBuilder.append("\n"); //new line every 80 cells
             }
-            sBuilder.append(keys[i] == null ? "E" : "O");
+            sBuilder.append(keys[i] == null ? "E" : "O");  //E if empty, O if full
         }
         return sBuilder.toString();
-    }
+    }//toString method
 
+    /**
+     * calculates average search miss cost of a hashmap by 
+     * iterating through and calculating each cells cost, then 
+     * averaging them against table size
+     * 
+     * @return average search miss cost
+     */
     public double empiricalAverageSearchMissCost() {
         double totalMissCost = 0;
-
         for (int i = 0; i < tableSize; i++) {
             if (keys[i] == null) {
-                totalMissCost++;
+                totalMissCost++; //if cell is empty, cost is 1
             } else {
                 int j = 0;
                 while (keys[(i + j) % tableSize] != null && j != tableSize) {
-                    totalMissCost++;
-                    j++;
+                    totalMissCost++; //if cell is not empty, iterate until you find an empty one or complete a circuit of the map
+                    j++; 
                 }
                 totalMissCost++;
             }
         }
-        return totalMissCost / tableSize;
-    }
+        return totalMissCost / tableSize; //total / tablesize to return average
+    }//empiricalAverageSearchMissCost method
 
+    /**
+     * translates a string into a hashmap and then calls empiricalAverageSearchMissCost()
+     * to get the average miss cost
+     * 
+     * @param str string to calculate miss cost
+     * @return miss cost
+     */
     public static double empiricalAverageSearchMissCost(String str) {
         TitanProbeHashMap<Integer, Integer> stringTestMap = new TitanProbeHashMap<>(str.length());
+        char full = 'O'; //full character is O
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'O'){
+            if (str.charAt(i) == full){ //if it's full put it into the map
                 stringTestMap.put(i, i);
             }
         }
         return stringTestMap.empiricalAverageSearchMissCost();
-    }
-
-
-}
+    }//empiricalAverageSearchMissCost
+}//TitanProbeHashMap class
